7f53ffa001e15fc319f54721b182a469ac48d7f5
Konrad <131642331+Uran16@users.noreply.github.com>
2026-08-26 19:40:23 +0200

    Update makeRobotAccountant.js

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,22 @@
 'use strict';
 
 /**
- *
  * @return {function}
  */
-
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  return function(a) {
+    return function(b) {
+      calls += 1;
+
+      if (calls > 3 && calls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
