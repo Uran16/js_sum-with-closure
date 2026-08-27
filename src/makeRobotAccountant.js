ef73eb531d7c9d53e6cd9f9760c48b7c1e984f0a
Konrad <131642331+Uran16@users.noreply.github.com>
2026-08-27 16:41:38 +0200

    Fix makeRobotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -1,12 +1,19 @@
 'use strict';
 
-/**
- *
- * @return {function}
- */
-
 function makeRobotAccountant() {
-  // write code here
+  let calls = 0;
+
+  return function getSum(a) {
+    return function addNumber(b) {
+      calls += 1;
+
+      if (calls > 3 && calls % 2 === 0) {
+        return 'Bzzz... Error!';
+      }
+
+      return a + b;
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
